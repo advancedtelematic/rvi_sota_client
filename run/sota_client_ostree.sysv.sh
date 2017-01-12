c478d83a0812366c73d6df2691dd66c97f6f5ac3
Jon Oster <jon@advancedtelematic.com>
2017-01-12 15:04:33 +0100

    PRO-2355 configure the certs file with cmdline option instead of sota.toml

--- run/sota_client_ostree.sysv.sh
+++ run/sota_client_ostree.sysv.sh
@@ -1,4 +1,4 @@
 /bin/sh
 
-RUST_LOG=debug /usr/bin/sota_client --config /sysroot/boot/sota.toml
+RUST_LOG=debug /usr/bin/sota_client --config /sysroot/boot/sota.toml --device-certificates-path /usr/etc/sota_certificates --device-package-manager ostree
 
