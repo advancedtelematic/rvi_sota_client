6fcd19359e9925816e4fea36b2343fabac556a07
OYTIS <tossel@gmail.com>
2017-03-29 12:31:21 +0200

    Whitespace matters

--- generate_yocto_deps.sh
+++ generate_yocto_deps.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-cat Cargo.lock | sed -e '1,/metadata/ d' Cargo.lock | awk '{print "crate://crates.io/"$2 "/" $3"\\"}'
+cat Cargo.lock | sed -e '1,/metadata/ d' Cargo.lock | awk '{print "crate://crates.io/"$2 "/" $3" \\"}'
